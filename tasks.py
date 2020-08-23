59b329e0c2bda064fa656ffa00799889b0227510
Steven Ondieki omwange <ondiekisteven@users.noreply.github.com>
2020-08-23 15:25:05 +0300

    removed worker from Procfile

--- tasks.py
+++ tasks.py
@@ -1,3 +1,5 @@
+import os
+from spotdl import Spotdl
 import celery
 from timeloop import Timeloop
 from datetime import timedelta
@@ -5,7 +7,7 @@ from Bot import WaBot
 
 
 tl = Timeloop()
-app = celery.Celery("background messages")
+app = celery.Celery("tasks")
 
 REDIS_URL = 'redis://h:p19ab68194c2bd67b1f15134ed24d3a4fc0464585da96534f61f933ffb0007495@ec2-23-23-149-97.compute-1' \
             '.amazonaws.com:27019 '
@@ -20,6 +22,24 @@ bot = WaBot({"messages": ['1', '2']})
 @tl.job(interval=timedelta(seconds=53))
 def job_every_minute():
     bot.send_message('254745021668@c.us', '.')
+
+
+@app.task
+def download_audio(self, song, user):
+    path = f'music/{user}'
+    if not os.path.exists(path):
+        print("Directory not found, Creating...")
+        os.mkdir(path)
+    args = {
+        "song": [song],
+        'output_file': path + '/{artist} - {track-name}.{output-ext}'
+    }
+    print(f'Downloading {song}')
+    self.send_message(f'{user}"c.us', 'Downloading your song...')
+    with Spotdl(args) as spotdl_handler:
+        spotdl_handler.match_arguments()
+
+    return path
 
 
 @app.task
