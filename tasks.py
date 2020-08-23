30dbb42295a1390225f94cbe291dc3978445f9a2
Steven Ondieki omwange <ondiekisteven@users.noreply.github.com>
2020-08-23 08:49:09 +0300

    changed REDIS_URL to local from env

--- tasks.py
+++ tasks.py
@@ -6,8 +6,10 @@ from datetime import timedelta
 tl = Timeloop()
 app = celery.Celery("background messages")
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+REDIS_URL = 'redis://h:p19ab68194c2bd67b1f15134ed24d3a4fc0464585da96534f61f933ffb0007495@ec2-23-23-149-97.compute-1.amazonaws.com:27019'
+
+app.conf.update(BROKER_URL=REDIS_URL,
+                CELERY_RESULT_BACKEND=REDIS_URL)
 
 
 @tl.job(interval=timedelta(seconds=10))
