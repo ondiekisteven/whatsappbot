174f7f4189334e2938374753dc51045d4cfb185a
Steven Ondieki omwange <ondiekisteven@users.noreply.github.com>
2020-08-23 09:14:46 +0300

    editing timeloop function

--- tasks.py
+++ tasks.py
@@ -7,7 +7,8 @@ from Bot import WaBot
 tl = Timeloop()
 app = celery.Celery("background messages")
 
-REDIS_URL = 'redis://h:p19ab68194c2bd67b1f15134ed24d3a4fc0464585da96534f61f933ffb0007495@ec2-23-23-149-97.compute-1.amazonaws.com:27019'
+REDIS_URL = 'redis://h:p19ab68194c2bd67b1f15134ed24d3a4fc0464585da96534f61f933ffb0007495@ec2-23-23-149-97.compute-1' \
+            '.amazonaws.com:27019 '
 
 app.conf.update(BROKER_URL=REDIS_URL,
                 CELERY_RESULT_BACKEND=REDIS_URL)
@@ -16,7 +17,7 @@ app.conf.update(BROKER_URL=REDIS_URL,
 bot = WaBot({"messages": ['1', '2']})
 
 
-@tl.job(interval=timedelta(seconds=10))
+@tl.job(interval=timedelta(seconds=53))
 def job_every_minute():
     bot.send_message('254745021668@c.us', '.')
 
