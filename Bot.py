83929ad300f33c1bc0f7d208dea1c68d15d24a34
Steven Ondieki omwange <ondiekisteven@users.noreply.github.com>
2020-08-28 10:00:44 +0300

    update sending audio

--- Bot.py
+++ Bot.py
@@ -245,6 +245,7 @@ class WaBot:
                 return self.send_message(sid, 'Error downloading song')
             path = f'https://som-whatsapp.herokuapp.com/files/music/{get_phone(message)}/{song}'
             if os.path.exists(f'music/{get_phone(message)}/{song}'):
+                print(f"Song found in music/{get_phone(message)}/{song}")
                 audio_sending = self.send_file(sid, path, "audio.mp3", "audio")
                 print(f'sending audio -> {audio_sending}')
                 os.remove(f'music/{get_phone(message)}/{song}')
