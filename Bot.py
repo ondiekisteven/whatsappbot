1c6a1522f9ea6701b53504074a02eed5a53e4f47
steven <ondiekisteven1@gmail.com>
2020-10-04 12:46:19 +0300

    New token

--- Bot.py
+++ Bot.py
@@ -67,11 +67,8 @@ class WaBot:
 
     def __init__(self, json):
         self.message = json
-        # self.APIUrl = 'https://eu122.chat-api.com/instance177295/'
-        # self.token = 'hyw554es46ksmsck'
-        # -----TESTING URL AND TOKEN ----
-        self.APIUrl = 'https://eu56.chat-api.com/instance178096/'
-        self.token = 'yh6ty89tj7xpjdvz'
+        self.APIUrl = 'https://eu160.chat-api.com/instance179030/'
+        self.token = 'q57eg6f4aiktkhom'
         self.last_command = "last command"
 
     def get_song(self, path):
