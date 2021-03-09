65abdcfd29b9eb752f7493fc9241717e747fd852
Steven Ondieki omwange <ondiekisteven@users.noreply.github.com>
2021-03-09 11:09:42 +0300

    AUDIO FIX

--- Bot.py
+++ Bot.py
@@ -503,13 +503,9 @@ eg. group My Music Group
                     return ''
                 self.send_message(sid, "Downloading your song... please wait")
                 db.add_downloading_user(sid)
-                try:
-                    downloader = Downloader(sid, choice)
-                    audio_name = downloader.download_audio()
-                except Exception as e:
-                    print(e)
-                    return self.send_message(sid, "could not complete the download. Try registering yourself in the "
-                                                  "bot's inbox and try again")
+                downloader = Downloader(sid, choice)
+                audio_name = downloader.download_audio()
+
                 # audio_name = Converter(path).convert()
                 path = f'{heroku_url}files/music/{get_phone(message)}/{audio_name}'
                 # path = f'localhost:5000/files/music/{get_phone(message)}/{audio_name}'
