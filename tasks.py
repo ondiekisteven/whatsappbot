562884eb454282aa5b228808e81a03f9fb6736dd
Steven Ondieki <ondiekisteven0@gmail.com>
2021-07-17 01:08:50 +0300

    remove default redis

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDIS_URL', 'redis://h:pffa2ba6288eb64eebf2bff97dc93ef56f156dd8a2758163f96fb52b5db387a5f@ec2-54-235-172-120.compute-1.amazonaws.com:10199')
+redis_url = os.getenv('REDIS_URL')
 
 conn = redis.from_url(redis_url)
 
